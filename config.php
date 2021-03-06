b1c3c4d12161e3d1c53d52998968706cc93ccf2c
eduardorj2019 <edu20000@gmail.com>
2021-03-06 11:46:04 -0300

    foi criado a pagina 404

--- config.php
+++ config.php
@@ -4,3 +4,4 @@ define('EMAIL','');
 define('HOST','');
 define('PASSWORD','');
 define('PORT','');
+define('BASE_URL','http://'.$_SERVER['HTTP_HOST']);
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,13 @@ use \PlugRoute\Http\RequestCreator;
 
 $route = new PlugRoute(new RouteContainer(), RequestCreator::create());
 
-$route->notFound(function(){
-    echo "404";
+$route->notFound(function() {
+   header('Location:/404');
 });
 
 $route->namespace('\\App\\Controllers', function($route) {
     $route->get('/', '\\HomeController@index'); 
+    $route->get('/404', '\\NotFoundController@index');
 });
 
 $route->namespace('\\App\\Controllers', function($route) {
